b9a190ddc50d9dc93c489693d61188901112c9f5
Rohan Shah <rjs971126@gmail.com>
2023-01-26 14:51:57 +0530

    data ingestion

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os,sys
 from Insurance.utils import get_collection_as_dataframe
 from Insurance.entity.config_entity import DataIngestionConfig
 from Insurance.entity import config_entity
+from Insurance.components.data_ingestion import DataIngestion
+
 
 
 '''def test_logger_and_exception():
@@ -24,6 +26,9 @@ if __name__=="__main__":
         training_pipeling_config=config_entity.TrainingPipelineConfig()
         data_ingetion_config = config_entity.DataIngestionConfig(training_pipeling_config=training_pipeling_config)
         print(data_ingetion_config.to_dict())
+
+        data_ingesion= DataIngestion(data_ingetion_config=data_ingetion_config)
+        data_ingesion_artifact= data_ingesion.intitate_data_ingestion()
 
     except Exception as e:
         print(e)
