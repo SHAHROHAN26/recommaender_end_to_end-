523e0d02cf8ec244e104b06fcc54e0c2422a299f
Rohan Shah <rjs971126@gmail.com>
2023-01-28 11:55:47 +0530

    data validation done

--- Insurance/components/data_ingestion.py
+++ Insurance/components/data_ingestion.py
@@ -12,49 +12,50 @@ from sklearn.model_selection import train_test_split
 
 class DataIngestion:        #data in to train test and validation 
 
-    def __init__(self,data_ingetion_config:config_entity.DataIngestionConfig):
+    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
         try:
-            self.data_ingetion_config=data_ingetion_config
+            self.data_ingestion_config=data_ingestion_config
         except Exception as e:
             raise InsuranceException(e,sys)
 
-    def intitate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
+    def initiate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
         try:
             logging.info("export data as pandas  DataFrame")        
             df:pd.DataFrame = utils.get_collection_as_dataframe(
-                database_name=self.data_ingetion_config.database_name,
-                collection_name=self.data_ingetion_config.collection_name)
+                database_name=self.data_ingestion_config.database_name,
+                collection_name=self.data_ingestion_config.collection_name)
             logging.info("save data in feature store")
             #replace na with NAN
             df.replace(to_replace="na",value=np.NAN,inplace=True)
             #save data in feature store
             logging.info("create feature store folder of not available")
 
-            feature_store_dir=os.path.dirname(self.data_ingetion_config.feature_store_file_path)
+            feature_store_dir=os.path.dirname(self.data_ingestion_config.feature_store_file_path)
             os.makedirs(feature_store_dir,exist_ok=True)
 
             logging.info("save df to feature store folder ")
 
-            df.to_csv(path_or_buf=self.data_ingetion_config.feature_store_file_path,index=False, header=True)
+            df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index=False, header=True)
 
             logging.info("spliting data into train and test")
-            train_df,test_df=train_test_split(df,test_size=self.data_ingetion_config.test_size,random_state=1)
+            train_df,test_df=train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=1)
 
             logging.info("create dataset dir folder if not exists")
-            dataset_dir = os.path.dirname(self.data_ingetion_config.train_file_path)
+            dataset_dir = os.path.dirname(self.data_ingestion_config.train_file_path)
             os.makedirs(dataset_dir, exist_ok=True)
 
             logging.info("save datatset to feature store folder")
-            train_df.to_csv(path_or_buf=self.data_ingetion_config.train_file_path,index=False, header= True)
-            test_df.to_csv(path_or_buf=self.data_ingetion_config.test_file_path,index=False, header= True)
+            train_df.to_csv(path_or_buf=self.data_ingestion_config.train_file_path,index=False, header= True)
+            test_df.to_csv(path_or_buf=self.data_ingestion_config.test_file_path,index=False, header= True)
 
             #prepare artifact folder
 
             data_ingesion_artifact = artifact_entity.DataIngestionArtifact(
-                feature_store_file_path=self.data_ingetion_config.feature_store_file_path,
-                train_file_path=self.data_ingetion_config.train_file_path,
-                test_file_path=self.data_ingetion_config.test_file_path
+                feature_store_file_path=self.data_ingestion_config.feature_store_file_path,
+                train_file_path=self.data_ingestion_config.train_file_path,
+                test_file_path=self.data_ingestion_config.test_file_path
             )
+            return data_ingesion_artifact
 
 
         except Exception as e:
--- Insurance/entity/config_entity.py
+++ Insurance/entity/config_entity.py
@@ -11,25 +11,24 @@ TEST_FILE_NAME = "test.csv"
 class TrainingPipelineConfig:
     def __init__(self):
         try:
-            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H$M%S')}")
-        except Exception as e:
-            raise InsuranceException(e,sys)
+            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
+        except Exception  as e:
+            raise InsuranceException(e,sys)    
 
 
 class DataIngestionConfig:
-    def __init__(self,training_pipeling_config: TrainingPipelineConfig):
+    def __init__(self,training_pipeline_config: TrainingPipelineConfig):
+        
         try:
             self.database_name="INSURANCE"
             self.collection_name="INSURANCE_PROJECT"
-            self.data_ingesion_dir= os.path.join(training_pipeling_config.artifact_dir,"data_ingetion")
-            self.feature_store_file_path=os.path.join(self.data_ingesion_dir,"feature_store",FILE_NAME)
-            self.train_file_path=os.path.join(self.data_ingesion_dir,"dataset",TRAIN_FILE_NAME) 
-            self.test_file_path=os.path.join(self.data_ingesion_dir,"dataset",TEST_FILE_NAME)
-            self.test_size=0.2
-
-
-        except Exception as e:
-            raise InsuranceException(e,sys)
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+            self.train_file_path = os.path.join(self.data_ingestion_dir,"dataset",TRAIN_FILE_NAME)
+            self.test_file_path = os.path.join(self.data_ingestion_dir,"dataset",TEST_FILE_NAME)
+            self.test_size = 0.2
+        except Exception  as e:
+            raise InsuranceException(e,sys)     
 
 
     #CONVER DATA IN DICT
@@ -39,3 +38,12 @@ class DataIngestionConfig:
         except Exception as e:
             raise InsuranceException(e,sys)
 
+class DataValidationConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.data_validation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_validation")
+        self.report_file_path=os.path.join(self.data_validation_dir, "report.yaml")
+        self.missing_threshold:float = 0.2
+        self.base_file_path = os.path.join("insurance.csv")
+
+    
